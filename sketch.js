32a5f754391761b2741758e1afeb6412bc1c1dc3
IanGTZ <ianemmanuelgtz@gmail.com>
2025-02-27 22:41:32 +0000

    Uptade

--- sketch.js
+++ sketch.js
@@ -1,14 +1,38 @@
+let foto
+
 function preload() {
   // put preload code here
+  foto = loadImage('./images/images.jpg')
 }
 
 function setup() {
   // put setup code here
   createCanvas(800,800)
+  textFont('Stencil')
+  textSize(24)
+textAlign(CENTER)
+textStyle(BOLD)
+  noLoop()
+  
 }
 
 function draw() {
   // put drawing code here
   background(255)
-  rect(100,100,200,200)
+  image(foto,0,0,800,800)
+
+  foto.loadPixels()
+  let numPixeles = foto.width * foto.height*4
+  for (let i = 0; i<numPixeles; i+=4)
+  {
+    foto.pixels[i] = 255 - foto.pixels[i]
+    foto.pixels[i+1] = 255 - foto.pixels[i+1] 
+    foto.pixels[i+2] = 255 - foto.pixels[i+2]
+
+  }
+  foto.updatePixels()
+  image(foto,0,0,800,800)
+  fill(0)
+  text("Evil RBD SI o NO?",width/2,30)
+  text("NO ME SALVES", width/2,700)
 }
